aa358894ae1a0d5993f5e4232d644083a547b685
Duvan Andrés Contreras Franco <111651526+Duvan-softwer2222@users.noreply.github.com>
2025-02-06 12:36:30 -0500

    Add files via upload

---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace PrimerProyecto
+{
+    public static class Logger {
+        public static void Log(string message){
+            Console.WriteLine(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("¿Como te llamas?");
+            string nombre = Console.ReadLine() ?? "Invitado";
+            Logger.Log($"Hola , {nombre}");
+        }
+    }
+}
